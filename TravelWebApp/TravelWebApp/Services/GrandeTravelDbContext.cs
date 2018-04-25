38000a8905b736a853353a2878b0dfbc8cde8e8b
luisdvalle <jums.delvalle@gmail.com>
2018-04-25 17:42:53 +1000

    Adding reference to DataService Library
    Also, adding GrandeTravel Db Context class

---
+++ TravelWebApp/TravelWebApp/Services/GrandeTravelDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DataService;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelWebApp.Services
+{
+    public class GrandeTravelDbContext : AppDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=GrandeTravelDB; Trusted_Connection=True");
+        }
+    }
+}
