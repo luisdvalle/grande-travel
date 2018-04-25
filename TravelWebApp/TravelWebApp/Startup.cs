49ab1f797748e8c141b289caf034aff1737eec3a
luisdvalle <jums.delvalle@gmail.com>
2018-04-25 17:43:44 +1000

    Configuring Identity Services

--- TravelWebApp/TravelWebApp/Startup.cs
+++ TravelWebApp/TravelWebApp/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using TravelWebApp.Services;
 
 namespace TravelWebApp
 {
@@ -15,6 +12,19 @@ namespace TravelWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddIdentity<IdentityUser, IdentityRole>
+            (
+                config =>
+                {
+                    config.User.RequireUniqueEmail = true;
+                    config.Password.RequireDigit = true;
+                    config.Password.RequiredLength = 6;
+                    config.Password.RequireLowercase = true;
+                    config.Password.RequireUppercase = true;
+                    config.Password.RequireNonAlphanumeric = true;
+                }
+            ).AddEntityFrameworkStores<GrandeTravelDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
