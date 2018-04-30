a4dce469c9d4083ea3e47497d790da6deaeb009d
luisdvalle <jums.delvalle@gmail.com>
2018-04-30 22:19:18 +1000

    Creating Data Service for custom tables

---
+++ DataService/DataService/DataService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataService
+{
+    public class DataService<T> : IDataService<T> where T : class
+    {
+        private AppDbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public DataService()
+        {
+            _dbContext = new AppDbContext();
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            _dbSet.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.FirstOrDefault(predicate);
+        }
+
+        public IEnumerable<T> Query(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ DataService/DataService/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataService
+{
+    public interface IDataService<T>
+    {
+        IEnumerable<T> GetAll();
+        void Create(T entity);
+        T GetSingle(Expression<Func<T, bool>> predicate);
+        void Update(T entity);
+        IEnumerable<T> Query(Expression<Func<T, bool>> predicate);
+        void Delete(T entity);
+    }
+}
