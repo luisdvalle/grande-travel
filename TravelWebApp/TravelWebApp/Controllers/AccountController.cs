65be52e575ac0b00976db75b3818ed152245a20c
luisdvalle <jums.delvalle@gmail.com>
2018-04-30 22:21:47 +1000

    Adding Login and Update Profile processes

--- TravelWebApp/TravelWebApp/Controllers/AccountController.cs
+++ TravelWebApp/TravelWebApp/Controllers/AccountController.cs
@@ -11,10 +11,12 @@ namespace TravelWebApp.Controllers
     public class AccountController : Controller
     {
         private UserManager<IdentityUser> _userManagerService;
+        private SignInManager<IdentityUser> _signInManagerService;
 
-        public AccountController(UserManager<IdentityUser> userManagerService)
+        public AccountController(UserManager<IdentityUser> userManagerService, SignInManager<IdentityUser> signInManagerService)
         {
             _userManagerService = userManagerService;
+            _signInManagerService = signInManagerService;
         }
 
         [HttpGet]
@@ -46,6 +48,65 @@ namespace TravelWebApp.Controllers
                         ModelState.AddModelError("", error.Description);
                     }
                 }
+            }
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            AccountLoginViewModel vm = new AccountLoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountLoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var results = await _signInManagerService.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, false);
+
+                if (results.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(vm.ReturnUrl))
+                    {
+                        return RedirectToAction(vm.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Username or Password incorrect");
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManagerService.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateDetails(AccountUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                
             }
 
             return View(vm);
---
+++ TravelWebApp/TravelWebApp/ViewModels/AccountUpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelWebApp.ViewModels
+{
+    public class AccountUpdateViewModel
+    {
+        public int ProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+            
+    }
+}
