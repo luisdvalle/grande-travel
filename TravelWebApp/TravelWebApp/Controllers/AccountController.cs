2094467fc328f160f924a36a558023dab555fc81
luisdvalle <jums.delvalle@gmail.com>
2018-05-09 19:49:12 +1000

    Adding logic to update user profile

--- TravelWebApp/TravelWebApp/Controllers/AccountController.cs
+++ TravelWebApp/TravelWebApp/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TravelWebApp.Models;
@@ -14,16 +15,20 @@ namespace TravelWebApp.Controllers
     {
         private UserManager<IdentityUser> _userManagerService;
         private SignInManager<IdentityUser> _signInManagerService;
+        private RoleManager<IdentityRole> _roleManagerService;
         private IDataService<Profile> _profileDataService;
+        private const string CUSTOMER_ROLE_NAME = "Customer";
 
         public AccountController(
-            UserManager<IdentityUser> userManagerService, 
+            UserManager<IdentityUser> userManagerService,
             SignInManager<IdentityUser> signInManagerService,
+            RoleManager<IdentityRole> roleManagerService,
             IDataService<Profile> profileDataService
             )
         {
             _userManagerService = userManagerService;
             _signInManagerService = signInManagerService;
+            _roleManagerService = roleManagerService;
             _profileDataService = profileDataService;
         }
 
@@ -45,13 +50,23 @@ namespace TravelWebApp.Controllers
 
                 if (result.Succeeded)
                 {
+                    // Add default role.
+                    IdentityRole checkRole = await _roleManagerService.FindByNameAsync(CUSTOMER_ROLE_NAME);
+                    if (checkRole != null)
+                    {
+                        await _userManagerService.AddToRoleAsync(user, CUSTOMER_ROLE_NAME);
+                    }
+                    else
+                    {
+                        await _roleManagerService.CreateAsync(new IdentityRole(CUSTOMER_ROLE_NAME));
+                        await _userManagerService.AddToRoleAsync(user, CUSTOMER_ROLE_NAME);
+                    }
+
                     // Add default Profile.
                     Profile profile = new Profile
                     {
                         FirstName = "Enter your first name",
                         LastName = "Enter your last name",
-                        Email = vm.Email,
-                        PhoneNumber = "Enter your phone number",
                         UserId = user.Id
                     };
                     _profileDataService.Create(profile);
@@ -115,9 +130,24 @@ namespace TravelWebApp.Controllers
         }
 
         [HttpGet]
-        public IActionResult UpdateProfile()
+        [Authorize]
+        public async Task<IActionResult> UpdateProfile()
         {
-            return View();
+            // Get current user.
+            IdentityUser user = await _userManagerService.FindByNameAsync(User.Identity.Name);
+            // Get profile for current user.
+            var profileDb = _profileDataService.GetSingle(p => p.UserId == user.Id);
+            // Mappinng to vm.
+            AccountUpdateViewModel vm = new AccountUpdateViewModel
+            {
+                FirstName = profileDb.FirstName,
+                LastName = profileDb.LastName,
+                Email = (string.IsNullOrEmpty(user.Email)) ? "Enter your email address" : user.Email,
+                PhoneNumber = (string.IsNullOrEmpty(user.PhoneNumber)) ? "Enter your email address" : user.PhoneNumber,
+                ProfileId = profileDb.ProfileId
+            };
+
+            return View(vm);
         }
 
         [HttpPost]
@@ -125,7 +155,38 @@ namespace TravelWebApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                
+                // Get current user.
+                IdentityUser user = await _userManagerService.FindByNameAsync(User.Identity.Name);
+
+                user.Email = vm.Email;
+                user.PhoneNumber = vm.PhoneNumber;
+
+                // Update User.
+                var result = await _userManagerService.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    // Mapping to profile.
+                    Profile profile = new Profile
+                    {
+                        FirstName = vm.FirstName,
+                        LastName = vm.LastName,
+                        ProfileId = vm.ProfileId,
+                        UserId = user.Id
+                    };
+
+                    // Update Profile.
+                    _profileDataService.Update(profile);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
             }
 
             return View(vm);
--- TravelWebApp/TravelWebApp/Models/Profile.cs
+++ TravelWebApp/TravelWebApp/Models/Profile.cs
@@ -10,7 +10,5 @@ namespace TravelWebApp.Models
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
     }
 }
